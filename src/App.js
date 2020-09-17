374a67fcc570742e24bd74fbde6173ffd6a0f120
j-lindsey <j-lindsey@users.noreply.github.com>
2020-09-16 20:47:33 -0600

    added components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={particlesOptions}
+      />
       <Navigation />
       <Logo />
-      {/*<ImageLinkForm />
+      <Rank />
+      <ImageLinkForm />{/*}
       <FaceRecognition />*/}
     </div>
   );
