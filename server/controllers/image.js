288d566f52e9ace5efda6556ae401f4c0908224f
j-lindsey <j-lindsey@users.noreply.github.com>
2020-11-06 09:43:38 -0700

    update server file and api

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '65a4bc8d46ac4754b30e95f417ce0973'
+});
+
+const handleApiCall = (req, res) => {
+    console.log(req.body);
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
 const handleImage = (req, res, knex) => {
     const { id } = req.body;
     knex('users').where('id', '=', id)
@@ -9,6 +25,7 @@ const handleImage = (req, res, knex) => {
         .catch(err => res.status(400).json('unable to get entries'))
 }
 
-module.exports={
-    handleImage: handleImage
+module.exports = {
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
 };
